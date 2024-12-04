05b916b60ded8dc6bede7019c908ca6388dce330
ilo70 <ilhamrosa1@hotmail.com>
2024-12-04 10:15:49 +0100

    Add files via upload

---
+++ project 2/lektion 1a.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+{
+    Console.WriteLine("Hej");
+    Console.WriteLine("Jag lär mig C#");
+    Console.WriteLine("Detta är min första program");
+
+    int age = 54;
+    Console.WriteLine(age);
+
+    Console.Write("Skriv in ditt favoritfärg:");
+    String color = Console.ReadLine();
+    Console.WriteLine(color);
+
+}
+Console.Write("Skriv in ditt födelseår:");
+string input = Console.ReadLine();
+int birthYear = int.Parse(input);
+Console.WriteLine(2024 - birthYear);
+
+
+
